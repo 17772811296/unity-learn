cf0e5904a087730f4a5a3bc9f55de0ab6e4c84ef
17772811296 <wangdahuan@aliyun.com>
2021-07-20 21:40:18 +0800

    第一次提交

---
+++ Assets/Editor/CreateMyScript.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Text.RegularExpressions;
+using UnityEditor;
+using UnityEditor.ProjectWindowCallback;
+using UnityEngine;
+
+public class CreateMyScript
+{
+    private const string MY_SCRIPT_DEFAULT = "Assets/Editor/ScriptTemplates/C#Script-MyNewBehaviour.cs.txt";
+
+    [MenuItem("Assets/Create/C# MYScript",false,80)]
+    public static void CreaatScript()
+    {
+        string locationPath = GetSelectedPathOrFallback();
+        ProjectWindowUtil.StartNameEditingIfProjectWindowExists(0, ScriptableObject.CreateInstance<MyDoCreateScriptAsset>(), locationPath + "/MyNewBehaviourScript.cs",
+            null, MY_SCRIPT_DEFAULT);
+    }
+
+    public static string GetSelectedPathOrFallback()
+    {
+        string path = "Assets";
+        foreach (UnityEngine.Object obj  in Selection.GetFiltered(typeof(UnityEngine.Object),SelectionMode.Assets))
+        {
+            path = AssetDatabase.GetAssetPath(obj);
+            if (!string.IsNullOrEmpty(path)&&File.Exists(path))
+            {
+                path = Path.GetDirectoryName(path);
+                break;
+            }
+        }
+        return path;
+    }
+}
+
+class MyDoCreateScriptAsset : EndNameEditAction
+{
+    public override void Action(int instanceId, string pathName, string resourceFile)
+    {
+        UnityEngine.Object o = CreateScriptAssetsFromTemplate(pathName, resourceFile);
+        ProjectWindowUtil.ShowCreatedAsset(o);
+    }
+
+    internal static UnityEngine.Object CreateScriptAssetsFromTemplate(string pathName,string resourceFile)
+    {
+        string fullPath = Path.GetFullPath(pathName);
+        StreamReader streamReader = new StreamReader(resourceFile);
+        string text = streamReader.ReadToEnd();
+        streamReader.Close();
+        string fileNameWithoutExtension = Path.GetFileNameWithoutExtension(pathName);
+        //替换文件
+        text = Regex.Replace(text, "#NAME#", fileNameWithoutExtension);
+        bool encoderShouldEmitUTF8Identifier = true;
+        bool throwOnInvalidBytes = false;
+        UTF8Encoding encoding = new UTF8Encoding(encoderShouldEmitUTF8Identifier, throwOnInvalidBytes);
+        bool append = false;
+        StreamWriter streamWriter = new StreamWriter(fullPath, append, encoding);
+        streamWriter.Write(text);
+        streamWriter.Close();
+        AssetDatabase.ImportAsset(pathName);
+        return AssetDatabase.LoadAssetAtPath(pathName, typeof(UnityEngine.Object));
+    }
+}
+
---
+++ Assets/Scripts/FontRebuild/FontRebuild.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+//调整文字大小时候，由于重建字体贴图，造成文字花屏，通过监听场景文字重构
+//事件，整体刷新当前场景所有字体。
+public class FontRebuild : MonoBehaviour 
+{
+    private Font m_NeedRebuildFont = null;
+	void Start () 
+	{
+        Font.textureRebuilt += delegate (Font font)
+          {
+              m_NeedRebuildFont = font;
+          };
+	}
+	
+	void Update () 
+	{
+        if (m_NeedRebuildFont)
+        {
+            //找到场景中所有Text，重新刷新
+            Text[] texts = GameObject.FindObjectsOfType<Text>();
+            if (texts!=null)
+            {
+                foreach (Text item in texts)
+                {
+                    if (item.font==m_NeedRebuildFont)
+                    {
+                        item.FontTextureChanged();
+                    }
+                }
+            }
+            m_NeedRebuildFont = null;
+        }
+	}
+}
