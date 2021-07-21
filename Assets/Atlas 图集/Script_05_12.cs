55c07ec4f03cad50641d2541f425e72efb506b3d
17772811296 <wangdahuan@aliyun.com>
2021-07-21 23:27:39 +0800

    Canvas组件和Atals图集

---
+++ Assets/Atlas 图集/Script_05_12.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.U2D;
+using UnityEngine.UI;
+
+public class Script_05_12 : MonoBehaviour 
+{
+    public Image image = null;
+    private SpriteAtlas m_SpriteAtlas = null;
+	void Start () 
+	{
+        m_SpriteAtlas = Resources.Load<SpriteAtlas>("New Sprite Atlas");
+	}
+
+    private void OnGUI()
+    {
+        if (GUILayout.Button("<size=80>更换 Sprite</size>"))
+        {
+            image.sprite = m_SpriteAtlas.GetSprite("unity");
+        }
+    }
+    void Update () 
+	{
+		
+	}
+}
